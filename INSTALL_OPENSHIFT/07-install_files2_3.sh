e3056befb39430974f78341530d9269823220280
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-07-19 11:52:50 +0200

    Create 07-install_files2_3.sh

---
+++ INSTALL_OPENSHIFT/07-install_files2_3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VAR="/root/pull-secret"
+if [[ -f $VAR ]]
+then
+
+        FILE=`cat $VAR`
+        ssh_var=`cat ~/.ssh/install_openshift.pub`
+
+        sed -i "s/ocp.lan/ocp.annuaire.groupe.local/g" makeDirectory/ocp-install/install-config.yaml
+        sed -i "s/{\"auths\": ...}/$FILE/g" makeDirectory/ocp-install/install-config.yaml
+        sed -i "s/ssh-ed25519 AAAA.../$ssh_var/g" makeDirectory/ocp-install/install-config.yaml
+else
+        echo " pull-secret is missing"
+fi
