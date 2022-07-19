20b5d28a968a235b7bcf6b819c92d02f5ba285cb
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-07-19 15:14:40 +0200

    Update and rename sshConnect to sshConnect.sh

--- INSTALL_OPENSHIFT/sshConnect.sh
+++ INSTALL_OPENSHIFT/sshConnect.sh
@@ -8,5 +8,18 @@ read -p " Give the name for new ssk-key ! " sshKeyName
  sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
  sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
   
+ echo "| Restart du service sshd >>" 
  sudo systemctl restart sshd
+ sudo systemctl status sshd
                                
+              
+ read -p "| Change  User password ? >>" pass
+ if [[ $pass == "yes" ]] || [[ $pass == "y" ]]
+ then
+   sudo passwd $pass
+ elif [[ $pass == "no" ]] || [[ $pass == "n" ]]
+ then
+   echo " You choose to not change the USER password "
+ else
+    echo " You've tape the wrong key "
+ fi
