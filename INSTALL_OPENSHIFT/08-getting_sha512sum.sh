28ac53f0ec8824986b339042694c126e3523a1e5
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-07-20 11:00:27 +0200

    Update 08-getting_sha512sum.sh

--- INSTALL_OPENSHIFT/08-getting_sha512sum.sh
+++ INSTALL_OPENSHIFT/08-getting_sha512sum.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 
-############################# BEGIN  OF getting sha512sum of .ign files ############################################
+############################# BEGIN  OF getting sha512sum of .ign files  ############################################
 #|||=>
 
 # Getting sha512sum of bootstrap.ign file 
 echo " | Getting sha512sum of bootstrap.ign file  >>"
 sha512sum ~/makeDirectory/ocp-install/bootstrap.ign > sha_boot_ori
 cat sha_boot_ori | cut -d " " -f1 > sha_boot_final
+cat sha_boot_final
 # Getting sha512sum of master.ign file 
 echo " | Getting sha512sum of master.ign file  >>"
 sha512sum ~/makeDirectory/ocp-install/master.ign  > sha_master_ori
 cat sha_master_ori | cut -d " " -f1 > sha_master_final
+cat sha_master_final
 # Getting sha512sum of worker.ign file 
 echo " | Getting sha512sum of worker.ign file  >>"
 sha512sum ~/makeDirectory/ocp-install/worker.ign  > sha_worker_ori
 cat sha_worker_ori | cut -d " " -f1 > sha_worker_final
+cat sha_worker_final
 
 ############################# END   OF getting sha512sum of .ign files ############################################
 #|||=>
