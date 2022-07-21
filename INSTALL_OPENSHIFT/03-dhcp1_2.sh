251917aa79ecc9fda48190df2dfd1bf1d2026113
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-07-22 01:53:03 +0200

    Update 03-dhcp1_2.sh

--- INSTALL_OPENSHIFT/03-dhcp1_2.sh
+++ INSTALL_OPENSHIFT/03-dhcp1_2.sh
@@ -15,9 +15,8 @@ sudo dnf install dhcp-server -y
 
 # Edit dhcp.conf file to match IPaddress and name of cluster machines
 echo " | Edit dhcp.conf file to match IPaddress and name of cluster machines >>"
-sudo cp ~/ocp4-metal-install/dhcpd.conf /etc/dhcp/dhcpd.conf
+sudo cp ~/makeDirectory/ocp4-metal-install/dhcpd.conf /etc/dhcp/dhcpd.conf
 sleep 2
-sudo vi /etc/dhcp/dhcpd.conf
 
 ############################# END   OF installation and configuration of DHCP PART(1/2) ############################################
 #|||=>
