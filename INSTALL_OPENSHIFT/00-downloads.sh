e9892d0b10e7c16ffbec9743070329c83426b215
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-07-21 20:26:51 +0200

    Update 00-downloads.sh

--- INSTALL_OPENSHIFT/00-downloads.sh
+++ INSTALL_OPENSHIFT/00-downloads.sh
@@ -2,13 +2,13 @@
 
 ############################# BEGIN  OF installation utils ############################################
 #|||=>
- mkdir ~/makeDirectory
- cd ~/makeDirectory
+ sudo mkdir ~/makeDirectory
+ sudo cd ~/makeDirectory
 
- dnf update
- dnf install git tree -y
+ sudo dnf update
+ sudo dnf install git tree -y
 
- git clone https://github.com/ryanhay/ocp4-metal-install.git
+ sudo git clone https://github.com/ryanhay/ocp4-metal-install.git
 
 ############################# END   OF installation and configuration of DHCP #########################################
 ############################# BEGIN OF DOWNLOADS OPENSHIFT-INSTALL OPENSHIFT-CLIENT , ISO #############################
@@ -18,29 +18,29 @@ then
     echo " Downloads directory already exists!"
     break
 else 
-     mkdir Downloads
+     sudo mkdir Downloads
      echo " | CHANGE DIRECTORY => Downloads >>"
-     cd Downloads
+     sudo cd Downloads
 fi
 
 
 # Opensfhit installer
 echo " | téléchargement de openshift installer >>"
- curl   https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-install-linux.tar.gz  | tar -xz
+sudo curl   https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-install-linux.tar.gz  | tar -xz
 
 
 # command line tool CLI
 echo " | téléchargement de command line tool CLI >>"
 #curl  https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-windows.zip 
 
- curl  https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz | tar -xz
+ sudo curl  https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz | tar -xz
 
 
 
 # Red Hat Enterprise Linux CoreOs(RHCOS)
 echo " | téléchargement de Red Hat Enterprise Linux CoreOs(RHCOS) >>"
 
-wget  https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/latest/rhcos-live.x86_64.iso
+sudo wget  https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/latest/rhcos-live.x86_64.iso
 
  cd ~
  tree makeDirectory
@@ -51,9 +51,9 @@ sleep 2
 
 #Checking  Version
 echo " | Checking  Version >>"
- mv makeDirectory/Downloads/oc makeDirectory/Downloads/kubectl /usr/local/bin
+ sudo mv makeDirectory/Downloads/oc makeDirectory/Downloads/kubectl /usr/local/bin
 
- oc version 
+ ./oc version 
  kubectl version
 
 ############################# END   OF CHECKING CLIENT TOOLS ARE WORKING ############################################
