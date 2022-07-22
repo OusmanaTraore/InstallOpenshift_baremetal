22038c60fd0954204036b04d2b9a42a6edf3284c
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-07-22 02:48:15 +0200

    Update 05-haproxy2_2.sh

--- INSTALL_OPENSHIFT/05-haproxy2_2.sh
+++ INSTALL_OPENSHIFT/05-haproxy2_2.sh
@@ -7,13 +7,28 @@
 
 # Firewall adding port for Haproxy
 echo " | Firewall adding port for Haproxy >>"
+echo " |Adding port 6443 zone insternal to firewalld >> "
 sudo firewall-cmd --add-port=6443/tcp --zone=internal --permanent # kube-api-server on control plane nodes
+
+echo " |Adding port 6443 zone external to firewalld >> "
 sudo firewall-cmd --add-port=6443/tcp --zone=external --permanent # kube-api-server on control plane nodes
+
+echo " |Adding port 22623 zone internal to firewalld >> "
 sudo firewall-cmd --add-port=22623/tcp --zone=internal --permanent # machine-config server
+
+echo " |Adding service http zone internal to firewalld >> "
 sudo firewall-cmd --add-service=http --zone=internal --permanent # web services hosted on worker nodes
+
+echo " |Adding service http zone external to firewalld >> "
 sudo firewall-cmd --add-service=http --zone=external --permanent # web services hosted on worker nodes
+
+echo " |Adding service https zone internal to firewalld >> "
 sudo firewall-cmd --add-service=https --zone=internal --permanent # web services hosted on worker nodes
+
+echo " |Adding service https zone external to firewalld >> "
 sudo firewall-cmd --add-service=https --zone=external --permanent # web services hosted on worker nodes
+
+echo " |Adding port 9000 zone external to firewalld >> "
 sudo firewall-cmd --add-port=9000/tcp --zone=external --permanent # HAProxy Stat
 
 #  Firewall reload
