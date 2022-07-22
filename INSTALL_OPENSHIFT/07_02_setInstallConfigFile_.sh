08b00e05b6fbf70d187503cd405709647a750182
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-07-22 03:52:34 +0200

    Update 07_02_setInstallConfigFile_.sh

--- INSTALL_OPENSHIFT/07_02_setInstallConfigFile_.sh
+++ INSTALL_OPENSHIFT/07_02_setInstallConfigFile_.sh
@@ -7,9 +7,10 @@ then
         FILE=`cat $VAR`
         ssh_var=`cat ~/.ssh/install_openshift.pub`
 
-        sed -i "s/ocp.lan/ocp.annuaire.groupe.local/g" makeDirectory/ocp-install/install-config.yaml
-        sed -i "s/{\"auths\": ...}/$FILE/g" makeDirectory/ocp-install/install-config.yaml
-        sed -i "s/ssh-ed25519 AAAA.../$ssh_var/g" makeDirectory/ocp-install/install-config.yaml
+        sed -i "s/ocp.lan/ocp.annuaire.groupe.local/g" ~/makeDirectory/ocp-install/install-config.yaml
+        sed -i "s/{\"auths\": ...}/$FILE/g" ~/makeDirectory/ocp-install/install-config.yaml
+        sed -i "s/ssh-ed25519 AAAA.../$ssh_var/g" ~/makeDirectory/ocp-install/install-config.yaml
+        cat ~/makeDirectory/ocp-install/install-config.yaml
 else
         echo " pull-secret is missing"
 fi
